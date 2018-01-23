068f7b318b383949668d6ae54c38a72364d4dc4c
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-01-23 12:23:08 -0800

    redooo

--- main.c
+++ main.c
@@ -3,17 +3,39 @@
 
 int main() {
   printf("----------------------------------------\n");
+  printf("----------------------------------------\n");
   int tacos[100];
   double burritos[200];
+  double burgers[100];
+  int hotdogs[200];
+  char cheetos[1000];
 
   //printf("main: printing free list\n");
   //print_free_list();
 
-  printf("main: allocating array of 100 integers\n");
-  void *p = my_malloc(100 * sizeof(int));
+  printf("main: allocating array of 100 doubles\n");
+  void* burgers_pointer = my_malloc(100 * sizeof(double));
+  printf("main: my memory starts at 0x%x\n", burgers_pointer);
+  print_free_list();
 
-  printf("main: allocating array of 300 doubles\n");
-  void *pp = my_malloc(200 * sizeof(double));
+  printf("main: allocating array of 100 integers\n");
+  void* tacos_pointer = my_malloc(100 * sizeof(int));
+  printf("main: my memory is at 0x%x\n", tacos_pointer);
+  print_free_list();
+
+  printf("main: allocating array of 1000 chars\n");
+  void *cheetos_pointer = my_malloc(1000 * sizeof(char));
+  printf("main: my memory is at 0x%x\n", cheetos_pointer);
+  print_free_list();
+
+  //printf("main: allocating array of 200 doubles\n");
+  //void *pppp = my_malloc(200 * sizeof(double));
+  //print_free_list();
+
+  //uses all free space
+  //printf("main: allocating array of 200 doubles\n");
+  //void *pp = my_malloc(200 * sizeof(double));
+  //print_free_list();
 
   //printf("main: printing free list\n");
   //print_free_list();
--- my_malloc.c
+++ my_malloc.c
@@ -2,88 +2,122 @@
 
 void* head = NULL;
 
-//TAKE space from free list, returns pointer to new user-block
-void* takeSpace(free_list_node *node, int size) {
-  free_list_node *new;
-  void* node_break;
-  node_break = node;
-
-  //set location of new free space
-  new = node_break + size + 16 + 1;
-  printf("ryder: new brk = 0x%x\n", new);
-  //set new free space block values
-  new->size = node->size - size - 16;
-  printf("ryder: new size = %d\n", new->size);
-  new->next = NULL;
-  printf("ryder: new next = %d\n", new->next);
-
-  return new;
-}
-
-void* allocateSpace() {
-  void* previous_break;
-  free_list_node *node;
-
-  previous_break = sbrk(SIZE);
-  printf("ryder: prev_brk = 0x%x\n", previous_break);
-
-  //set node pointer to start of new memory
-  node = previous_break;
-  //set nodes values
-  node->size = SIZE - 16;
-  printf("ryder: node size = %d\n", node->size);
-  node->next = NULL;
-  printf("ryder: node next = %d\n", node->next);
-
-  return node;
-}
-
-void *my_malloc(int size) {
-  free_list_node *node;
-  free_list_node *previous;
-  free_list_node *current;
-  void* previous_break;
-  void* return_break;
+//TAKE space from free list, returns pointer to requested user-block
+void* useFreeSpace(free_list_node* old_free_block, free_list_node* last, int size) {
+  printf("-USEFREEPACE: taking %d from block @ 0x%x\n", size, old_free_block);
+  free_list_node* new_free_block;
+  free_list_node* new_user_block;
 
 
-
-  printf("my_malloc: called with size = %d\n", size);
-  printf("ryder: head = 0x%x\n", head);
-
-  //SCAN
-  //if no free list exists
-  if (head == NULL) {
-    printf("my_malloc: allocating new free list\n");
-
-    node = allocateSpace();
-
-    head = takeSpace(node, size);
-
-    return_break = previous_break;
-
-
+  //setup new free block
+  new_free_block = (void*)old_free_block + size + sizeof(free_list_node);    //+16??   sizeof(free_list_node)
+  new_free_block->size = old_free_block->size - size - sizeof(free_list_node);
+  new_free_block->next = old_free_block->next;
+  if (last == NULL) {
+    head = new_free_block;
   } else {
-    //set current as first node for scanning
-    current = head;
-    //check and move current until a big enough block or on last node
-    while (current->next != NULL) {
-
-      if (current->size >= size) {
-        node = current;
-      }
-      current = current->next;
-
+    if (last != old_free_block) {
+      last->next = new_free_block;
     }
-    //if fits in last block, take free space
-    if (current->size >= size) {
-      node = current;
-      printf("bingo\n");
-      //
-    //else, doesn't fit in any blocks, allocated more
+  }
+
+  //setup user's requested block
+  new_user_block = (void*)old_free_block;
+  new_user_block->size = size;
+  new_user_block->next = NULL;
+
+  printf("-USEFREEPACE: free block updated with size %d and @ 0x%x\n", new_free_block->size, new_free_block);
+
+  return (void*)new_user_block + 16;
+}
+
+void* allocateSpace(free_list_node* last) {
+  printf("-ALLOCATESPACE: allocating new free block\n");
+  free_list_node* new_block;
+
+  void* previous_break = sbrk(SIZE);
+
+  //create and setup new free block
+  new_block = previous_break;
+  new_block->size = SIZE - 16;
+  new_block->next = NULL;
+
+
+  printf("-ALLOCATESPACE: new free block created with size %d and @ 0x%x\n", new_block->size, new_block);
+
+  //update last's pointer
+  if (last != NULL) {
+    last->next = new_block;
+  }
+
+  return previous_break;
+}
+
+void* my_malloc(int size) {
+  void* user_space_break;
+  void* previous_break;
+
+  printf("MY_MALLOC: called with size = %d\n", size);
+  printf("-my_malloc: head = 0x%x\n", head);
+
+
+  //if no free list
+  if (head == NULL) {
+    printf("-my_malloc: no free list\n");
+
+    //returns previous break, takes last of free list
+    previous_break = allocateSpace(NULL);
+    free_list_node* free_block = previous_break;
+
+    head = previous_break;
+
+    //returns location of user's requested space,
+    user_space_break = useFreeSpace(free_block, NULL, size);
+
+
+  //if free list
+  } else {
+    printf("-my_malloc: scanning free list...");
+    free_list_node* free_block;
+    free_list_node* last;
+    free_list_node* current = head;
+
+    //scan
+    while (current->next != NULL) {
+      if (current->size >= size + 16) {
+        printf("found space in free list\n");
+        free_block = current;
+      }
+      last = current;
+      current = current->next;
+    }
+    if (current->size >= size + 16) {
+      printf("found space in free list\n");
+      free_block = current;
+      last = NULL;
+    }
+    //end scan
+
+    if (free_block != NULL) {
+      printf("-my_malloc: free space is @ 0x%x\n", free_block);
+
+      printf("last = 0x%x\n", last);
+
+      //returns location of user's requested space
+      user_space_break = useFreeSpace(free_block, last, size);
 
     } else {
-      printf("ahhh shit, need more memory\n");
+      printf("not enough space in free list\n");
+      last = current;
+
+      //returns previous break
+      previous_break = allocateSpace(last);
+      free_list_node* free_block = previous_break;
+
+      //returns location of user's requested space
+      user_space_break = useFreeSpace(free_block, last, size);
     }
+
 
   }
 
@@ -91,17 +125,32 @@ void *my_malloc(int size) {
 
 
 
-  return return_break;
+  return user_space_break;
 }
 
-void my_free(void *ptr) {
+void my_free(void* ptr) {
 
 
 }
 
 void print_free_list() {
-  printf("==========FREE LIST==============================\n");
-  printf("NODE #  |      ADDRESS  |   SIZE  |      NEXT\n");
-  printf("=================================================\n");
+  free_list_node* current;
+  int number;
+  current = head;
+
+  if (current != NULL) {
+    printf("==========FREE LIST================================\n");
+    printf("  NODE #  |      ADDRESS  |    SIZE  |         NEXT\n");
+    while (current->next != NULL) {
+      printf("       %d  |  0x  %7x  |    %4d  |  0x  %7x\n", number, current, current->size, current->next);
+      current = current->next;
+    }
+    printf("       %d  |  0x  %7x  |    %4d  |  0x  %7x\n", number, current, current->size, current->next);
+    printf("===================================================\n");
+  } else {
+    printf("==========FREE LIST================================\n");
+    printf("  NODE #  |      ADDRESS  |    SIZE  |         NEXT\n");
+    printf("===================================================\n");
+  }
 
 }
